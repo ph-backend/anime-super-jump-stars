26db2ed75140f19c75ac52849a458c8739584e87
ph-backend <paulohenrique838383@gmail.com>
2026-03-14 10:49:49 -0300

    Fight Loop - 2

--- Anime Super Jump Stars.py
+++ Anime Super Jump Stars.py
@@ -140,9 +140,12 @@ while jotaro.life > 0 and dio.life > 0:
     show_status(jotaro, dio)
     show_skills(jotaro)
 
-    if jotaro.life <= 0:
-        print("\nJotaro was defeated!")
-        break
+    choice = input("Choose skill (0 for basic): ")
+    if choice == "3":
+        jotaro.attack(dio)
+    else:
+        idx = int(choice) - 1
+        jotaro.skills[idx].use(jotaro, dio)
 
 
 
